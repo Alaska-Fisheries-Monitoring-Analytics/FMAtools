cc4f1df21672b7db5667e1d41bc045c2937e60f5
Geoffrey Mayhew <geoff.mayhew@noaa.gov>
2024-02-13 16:00:44 -0800

    Replaced -package.Rd with one using the package's name

---
+++ R/FMAAnalystFunctions-package.R
@@ -0,0 +1,14 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dbplyr window_order
+#' @importFrom lubridate day
+#' @importFrom lubridate month
+#' @importFrom lubridate year
+#' @importFrom magrittr %>%
+#' @importFrom readxl excel_sheets
+#' @importFrom readxl read_excel
+#' @importFrom utils file_test
+## usethis namespace: end
+NULL
